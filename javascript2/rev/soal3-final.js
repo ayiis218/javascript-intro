ae221441b29eca6920b7381ad874adde48befe79
ayiis <76609039+ayiis218@users.noreply.github.com>
2022-08-30 15:28:07 +0700

    [revisi] validasi

--- javascript2/rev/soal3-final.js
+++ javascript2/rev/soal3-final.js
@@ -1,49 +1,48 @@
 //deklarasi data array
-let data  = [2, 13, 5, 9, 14, 7, 17]
-let data2 = [2, 25, 4]
-let data3 = [2, 25, 4, 14, 'pijar', 'camp', 8]
-function search (nilaiAwal, nilaiAkhir, array){
-    let seleksiSoal = console.log( `Seleksi Nilai (${nilaiAwal},${nilaiAkhir}`,array,`)` )            // soal seleksi
+let data = [2, 13, 5, 9, 14, 7, 17];
+let data2 = [2, 25, 4];
+let data3 = [2, 25, 4, 14, 'pijar', 'camp', 8];
+function search(nilaiAwal, nilaiAkhir, array) {
+   let selection = console.log(
+      `Seleksi Nilai (${nilaiAwal},${nilaiAkhir}`,
+      array,
+      `)`
+   ); // soal seleksi
 
-    let nilaiA = typeof nilaiAwal === typeof 'string'
-    let nilaiAk = typeof nilaiAkhir === typeof 'string'
+   let score = typeof nilaiAwal !== 'number' || typeof nilaiAkhir !== 'number'; // pengecekan tipe data nilai
+   let arrayData = !array.every((index) => typeof index === 'number'); // pengecekan tipe data data array
 
-    let nilaiString = nilaiA || nilaiAk                                                 // pengecekan tipe data nilai
-    let arrayString = !array.every( index =>  typeof index === 'number')                // pengecekan tipe data data array
-    // validasi data string
-    if(nilaiString){
-        console.log('Data yang di inputkan harus bersifat number')
-    // validasi data string
-    }else if(arrayString){
-        console.log('Data array harus bersifat number')
-    // validasi jumlah data array
-    }else if(array.length <= 5){
-        console.log('Jumlah data array harus lebih dari 5')
-    // validasi perbandingan niali
-    }else if(nilaiAwal < nilaiAkhir){
-        // proses pencarian data dari parameter yang di input
-        let cari = array.filter( index => { return index > nilaiAwal && index < nilaiAkhir})
-            // mengurutkan data array hasil pencarian
-            cari.sort((a, b) => {return a - b})
-            // validasi data
-            if(cari.length !== 0){
-                seleksiSoal
-                console.log(cari)
-            }else{
-                seleksiSoal
-                console.log('Data tidak ditemukan', cari)
-            }
-    }else{
-        seleksiSoal
-        console.log("Nilai akhir harus lebih besar dari nilai awal")
-    }
+   if (score) {
+      console.log('Data yang di inputkan harus bersifat number');
+   } else if (arrayData) {
+      console.log('Data array harus bersifat number');
+   } else if (array.length <= 5) {
+      console.log('Jumlah data array harus lebih dari 5');
+   } else if (nilaiAwal < nilaiAkhir) {
+      let search = array.filter((index) => {
+         return index > nilaiAwal && index < nilaiAkhir;
+      });
+      search.sort((a, b) => {
+         return a - b;
+      });
+      if (search.length !== 0) {
+         selection;
+         console.log(search);
+      } else {
+         selection;
+         console.log('Data tidak ditemukan', search);
+      }
+   } else {
+      selection;
+      console.log('Nilai akhir harus lebih besar dari nilai awal');
+   }
 }
 
 //perintah untuk menjalankan program
-search(2,14,data)
-search(10,5,data)
-search(5, 17,data2)
-search(17,20,data)
-search('5', 20,data)
-search(5, '20',data)
-search(17, 20,data3)
+search(2, 14, data);
+search(10, 5, data);
+search(5, 17, data2);
+search(17, 20, data);
+search('5', 20, data);
+search(5, '20', data);
+search(17, 20, data3);
